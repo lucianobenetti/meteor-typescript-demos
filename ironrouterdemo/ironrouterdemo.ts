f7ca03b0818a350ee324f9606871358e3032b0cf
Olivier Refalo <orefalo@yahoo.com>
2013-09-26 18:55:59 -0400

    trying to get the ironrouter demo working.. think its an issue with the pkg itself

--- ironrouterdemo/ironrouterdemo.ts
+++ ironrouterdemo/ironrouterdemo.ts
@@ -1,114 +1,35 @@
-/// <reference path="packages/typescript-libs/lib.d.ts" />
 /// <reference path="packages/typescript-libs/meteor.d.ts" />
 /// <reference path="packages/typescript-libs/ironrouter.d.ts" />
+/// <reference path="packages/typescript-libs/node-fibers.d.ts" />
+/// <reference path="packages/typescript-libs/node.d.ts" />
 
-
-interface ItemsDAO {
-	_id?: string;
-	body:string;
-}
-
-declare var Items:Meteor.Collection<ItemsDAO>;
-declare var Subscriptions;
 declare var ItemsController;
 
-Items = new Meteor.Collection<ItemsDAO>('items');
-
-Router.map(function () {
-  this.route('home', {
-    path: '/'
-  });
-
-  this.route('items', {
-    controller: 'ItemsController',
-    action: 'customAction'
-  });
-});
-
-if (Meteor.isServer) {
-  var seed = function () {
-    Items.remove({});
-    for (var i = 0; i < 100; i++) {
-      Items.insert({body: 'Item ' + i});
-    }
-  };
-
-  Meteor.startup(seed);
-
-  var Future = Npm.require('fibers/future');
-
-  Meteor.publish('items', function () {
-    var future = new Future;
-
-    // simulate high latency publish function
-    Meteor.setTimeout(function () {
-      future.return(Items.find());
-    }, 2000);
-
-    return future.wait();
-  });
-}
-
 if (Meteor.isClient) {
-  Router.configure({
-    layout: 'layout',
-    notFoundTemplate: 'notFound',
-    loadingTemplate: 'loading'
-  });
+
+	Router.map(function() {
+		this.route('home', {
+			path: '/'
+		});
+
+		this.route('about');
+		this.route('post');
+
+	});
+
+	Router.configure({
+		layout: 'layout'
+	});
 
 
-  Subscriptions = {
-    items: Meteor.subscribe('items')
-  };
+	ItemsController = RouteController.extend({
+		template: 'post',
+		renderTemplates: {
+			'sidebar': { to: 'sidebar'},
+			'footer': { to: 'footer'}
+		},
+		data: {title: "Hello World"},
+		run: function() { console.log('running'); }
 
-
-//  ItemsController = RouteController.extend({
-//    template: 'items',
-//
-//    /*
-//     * During rendering, wait on the items subscription and show the loading
-//     * template while the subscription is not ready. This can also be a function
-//     * that returns on subscription handle or an array of subscription handles.
-//     */
-//
-//    waitOn: Subscriptions['items'],
-//
-//    /*
-//     * The data function will be called after the subscrition is ready, at
-//     * render time.
-//     */
-//
-//    data: function () {
-//      // we can return anything here, but since I don't want to use 'this' in
-//      // as the each parameter, I'm just returning an object here with a named
-//      // property.
-//      return {
-//        items: Items.find()
-//      };
-//    },
-//
-//    /*
-//     * By default the router will call the *run* method which will render the
-//     * controller's template (or the template with the same name as the route)
-//     * to the main yield area {{yield}}. But you can provide your own action
-//     * methods as well.
-//     */
-//    customAction: function () {
-//
-//      /* render customController into the main yield */
-//      this.render('items');
-//
-//      /*
-//       * You can call render multiple times. You can even pass an object of
-//       * template names (keys) to render options (values). Typically, the
-//       * options object would include a *to* property specifiying the named
-//       * yield to render to.
-//       *
-//       */
-//      this.render({
-//        itemsAside: { to: 'aside', waitOn: false, data: false },
-//        itemsFooter: { to: 'footer', waitOn: false, data: false }
-//      });
-//    }
-//  });
+	});
 }
